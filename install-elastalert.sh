f3e36bb2093d7a299b21e624f269a259d3e6c7e7
Ved Sachdeva <mrvedsachdeva@gmail.com>
2018-06-05 20:11:32 +0530

    update install-elastalert.sh
    added command to clone elastalert and update requirements curl

--- install-elastalert.sh
+++ install-elastalert.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-wget https://github.com/mrved/scripts/raw/master/get-pip.py
+curl https://github.com/mrved/scripts/raw/master/get-pip.py -o get-pip.py
+curl https://raw.githubusercontent.com/mrved/scripts/master/requirements6.2.txt -o requirements.txt
 sudo python get-pip.py
 pip install --user "setuptools>=11.3"
 sudo yum install python-devel
 pip install --user -r requirements.txt
+sudo yum install git
+git clone https://github.com/Yelp/elastalert.git

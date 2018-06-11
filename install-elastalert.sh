c484ba9aface22ea469f0eac4ecf835766773c09
Ved Sachdeva <mrvedsachdeva@gmail.com>
2018-06-11 11:03:51 +0530

    update

--- install-elastalert.sh
+++ install-elastalert.sh
@@ -3,9 +3,9 @@ curl https://raw.githubusercontent.com/mrved/scripts/master/get-pip.py -o get-pi
 curl https://raw.githubusercontent.com/mrved/scripts/master/requirements6.2.txt -o requirements.txt
 sudo python get-pip.py
 pip install --user "setuptools>=11.3"
-sudo -y yum install python-devel
+sudo yum install python-devel
 pip install --user -r requirements.txt
-sudo -y yum install git
+sudo yum install git
 git clone https://github.com/Yelp/elastalert.git
 rm -rf get-pip.py
 rm -rf requirements.txt
